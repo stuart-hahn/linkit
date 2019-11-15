8b67b813562aaad917cbe10c182c84ddcdba5032
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-15 15:56:11 -0500

    add new link form and validations to model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,7 @@
 class Link < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :title, presence: true
+  validates :url, uniqueness: { case_sensitive: false }, allow_blank: true
 end
