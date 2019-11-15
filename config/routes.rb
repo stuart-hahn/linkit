e0b0bc634067f3b13edde02f111453845d000cbc
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-15 15:17:36 -0500

    devise omniauth for github users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'callbacks/github'
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  
+  root to: "links#index"
+  resources :links
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
